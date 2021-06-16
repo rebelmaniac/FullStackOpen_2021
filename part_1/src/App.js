7ec27b25a5e32293956bd9a32dac7495e6c01e91
rebelmaniac <kesnerbd@gmail.com>
2021-06-16 13:04:15 -0400

    initial commit to feature/02

---
+++ part_1/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}> 
+        zero
+      </button>
+    </div>
+  )
+}
+
+export default App
